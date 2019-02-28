6d34a191546e7ccb93da791f0cef5549a4c6c3cc
HansZimmer5000 <michael.weitnau@live.de>
2019-02-28 23:02:59 +0100

    more stuff

--- main.py
+++ main.py
@@ -1,44 +1,7 @@
 import sys
-import os
+import dirTree
 
-def calculateDirSizeInMb(dirFullPath):
-    size = 0
-    for (path, _, files) in os.walk(dirFullPath):
-        for file in files:
-            filename = os.path.join(path,file)
-            size += os.path.getsize(filename)
-    return round(size / (1024*1024), 2)
-
-def dirContainsSubDirs(directory):
-    for element in os.listdir(directory):
-        if(os.path.isdir(directory + "/" + element)):
-            return True
-    
-    return False
-
-def createDirTree(startfolderFullPath, depth):
-    if(depth >= 0):
-        if(dirContainsSubDirs(startfolderFullPath)):
-            size = calculateDirSizeInMb(startfolderFullPath)
-            date = None
-            subDirs = {}
-            for element in os.listdir(startfolderFullPath):
-                if(os.path.isdir(startfolderFullPath + "/" + element)):
-                    tmpSubdir = createDirTree(startfolderFullPath + "/" + element, depth - 1)
-                    subDirs = {**subDirs, **tmpSubdir}
-            return {startfolderFullPath: (size,date ,subDirs)}
-        else:
-            size = calculateDirSizeInMb(startfolderFullPath)
-            date = None
-            subDirs = {}
-            return {startfolderFullPath: (size, date, subDirs)}
-    else:
-        return {}
-
-
-if __name__ == "__main__":
-    arguments = sys.argv
-
+def gather_arguments(arguments):
     if(len(arguments) > 2):
         if(len(arguments) > 3):
             depth = int(arguments[3])
@@ -47,9 +10,23 @@ if __name__ == "__main__":
 
         startfolderFullPath1 = arguments[1]
         startfolderFullPath2 = arguments[2]
-        dirTree1 = createDirTree(startfolderFullPath1, depth)
-        dirTree2 = createDirTree(startfolderFullPath2, depth)
+        result = (startfolderFullPath1, startfolderFullPath2, depth)
+    else:
+        result = None
+
+    return result
+
+if __name__ == "__main__":
+    arguments = gather_arguments(sys.argv)
+
+    if(arguments is None):
+        print("To less / wrong arguments")
+
+    else:
+        startfolderFullPath1, startfolderFullPath2, depth = arguments
+
+        dirTree1 = dirTree.Create_dir_tree(startfolderFullPath1, depth)
+        dirTree2 = dirTree.Create_dir_tree(startfolderFullPath2, depth)
         print(dirTree1)
         print(dirTree2)
-    else:
-        print("Not two Startfolders given!")
+        dirTree.Compare_dir_tree(dirTree1, dirTree2)        
